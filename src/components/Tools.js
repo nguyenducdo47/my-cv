227823b742958b7c8a81d17e551572d3ac836daf
nguyenducdo47 <nguyenducdo009@gmail.com>
2024-11-19 00:39:17 +0700

    fix sidebar

--- src/components/Tools.js
+++ src/components/Tools.js
@@ -2,31 +2,22 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 const Tools = () => {
-  const { t } = useTranslation(); 
+  const { t } = useTranslation();
 
-  const tools = t('tools', { returnObjects: true }); 
+  const tools = t('tools', { returnObjects: true });
 
   return (
     <div>
-      <h4 className="mt-4 text-primary">{tools.title}</h4> 
-      <ul>
-        {Object.entries(tools).map(([category, toolsArray], index) => (
-          category !== 'title' && (
-            <li key={index} className="mt-2">
-              <strong>
-                {category
-                  .replace(/_/g, ' ') 
-                  .replace(/\b\w/g, char => char.toUpperCase())} 
-              </strong>
-              <ul>
-                {toolsArray.map((tool, idx) => (
-                  <li key={idx}>{tool}</li>
-                ))}
-              </ul>
-            </li>
-          )
-        ))}
-      </ul>
+      <h4 className="mt-4 text-primary">{tools.title}</h4>
+      {Object.entries(tools).map(([category, toolsArray], index) => (
+        category !== 'title' && (
+          <ul key={index} className="mt-2">
+            {toolsArray.map((tool, idx) => (
+              <li key={idx}>{tool}</li>
+            ))}
+          </ul>
+        )
+      ))}
     </div>
   );
 };
